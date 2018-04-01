f065f51102b73edc6c25390e4fe974f3e8f62675
Aghiles dziri <aghiles_dzii@hotmail.fr>
2018-04-01 14:41:09 +0200

    client console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static VelibSoapConsole.ServiceVelibClient service = new VelibSoapConsole.ServiceVelibClient();
+        private static string help = "Les commande sont suppoter en miniscule, masescule ou un melange des deux\n" +
+            "towns : rend la liste des ville qui sont disponible (qui ont des station)\n" +
+            "stations [town] : rend les stations de la ville demandé" +
+            "availablevelib [town] [station]: rend le nombre de vélo disponible dans une station données qui se trouve dans une ville.";
+        private static string error = "cette commande n'existe pas";
+        static void Main(string[] args)
+        {
+            service.getTowns();
+            Console.WriteLine("Bonjour ! Client Console");
+            Console.WriteLine("Help : pour voir les commande et à quoi elles servent");
+            string command;
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine().ToLower().Trim();
+                if (command.Split().Length != 1) {
+                    checkCommand(command);
+                }
+                else checkCommandUnaire(command);
+
+            } while (command != "exit" && command!= "quit");
+        }
+
+        private  static void checkCommandUnaire(string command)
+        {
+            switch (command)
+            {
+                case "help":
+                    Console.WriteLine(help);
+                    break;
+                case "towns":
+                    Console.Write(Towns());
+                    break;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                case "quit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine(command +":"+error);
+                    break;
+            }
+            
+        }
+        private static void checkCommand(string command)
+        {
+            string[] tab = command.Split(' ');
+            if (tab.Length == 3 && tab[0] == "availablevelib")
+            {
+                Console.WriteLine(AvailableVelib(tab[1].Trim(),tab[2].Trim()));
+            }
+            else if (tab.Length == 2 && tab[0].Trim() == "stations")
+            {
+                Console.Write(Stations(tab[1].Trim()));
+            }
+            else
+            {
+                string err = (tab.Length == 0) ? "merci de donner une commande vous avez entrer une ligne vide\n" : command + ": " + error;
+                Console.WriteLine(err);
+            }
+
+        }
+
+        private static string Stations(string town)
+        {
+            string print = "Les stations de la ville "+town+" :\n";
+            string[] station = service.getStations(town);
+            if(station == null)
+            {
+                return "La ville indiqué n'existe pas\n";
+            }
+            foreach (string str in station)
+            {
+                print += str + "\n";
+            }
+            return print;
+        }
+        private static string Towns()
+        {
+            string print = "Les villes :\n";
+            string[] town = service.getTowns();
+            if (town == null)
+            {
+                return "Error!!!!";
+            }
+            foreach (string str in town ){
+                print += str + "\n";
+            }
+            return print;
+        }
+        private static string AvailableVelib(string town,string station)
+        {
+            string print = "le nombre de velibs disponible est dans la station " + station + " qui se trouve dans la ville " + town + ": ";
+            int v = service.getAvailableBikes(town,station,0);
+            if (v == -1)
+            {
+                return "Assurez vous du nom de la ville et de la stations";
+            }
+            return print + v;
+        }
+       
+    }
+    
+}
--- VelibGateway/ServiceVelib.cs
+++ VelibGateway/ServiceVelib.cs
@@ -14,7 +14,7 @@ namespace VelibGateway
     {
         //la clé pour l'utilisation de l'API
         private static readonly string API_KEY = "3262b38e08b9a4da636a6611bf24c9de0fb93069";
-        private Dictionary<string, Dictionary<string, Tuple<int,int>>> cache = new Dictionary<string, Dictionary<string, Tuple<int,int>>>();
+        private static Dictionary<string, Dictionary<string, Tuple<int,int>>> cache = new Dictionary<string, Dictionary<string, Tuple<int,int>>>();
         public const int DelayMilliseconds = 10000;
         /**
          * toutes les stations de l'API
@@ -22,13 +22,18 @@ namespace VelibGateway
 
         public IList<string> getStations(string town)
         {
-
+            if (cache.ContainsKey(town) == false)
+            {
+                return null;
+            }
             if (cache[town].Count == 0)
             {
                 return getStationsService(town);
             }
             else
             {
+                if (cache[town] == null)
+                    return null;
                 return cache[town].Keys.ToList();
             }
         }
@@ -49,99 +54,147 @@ namespace VelibGateway
          **/
         public int getAvailableBikes(string town, string station, int time)
         {
-            int veloLibre = cache[town][station].Item1;
-        
-            if (DateTime.Now.Second - cache[town][station].Item2 <= time  && veloLibre != -1)
+            string sta = null;
+            int veloLibre = -1;
+            if (town == null || cache.ContainsKey(town) == false)
             {
-                return veloLibre;
+                return -1;
             }
-            veloLibre = getAvailableBikesService(town, station);
-            cache[town][station] = Tuple.Create(veloLibre, DateTime.Now.Second);
+            if (station != null) {
+                sta = cache[town].Keys.FirstOrDefault(k => k.Contains(station));
+                if (sta != null)
+                {
+                    veloLibre = cache[town][sta].Item1;
+                    if (DateTime.Now.Second - cache[town][sta].Item2 <= time && veloLibre != -1)
+                    {
+                        return veloLibre;
+                    }
+                }
+                else { sta = station; }
+            } else return -1;
+            veloLibre = getAvailableBikesService(town, sta);
+            if (veloLibre != -1)
+            {
+                sta = cache[town].Keys.FirstOrDefault(k => k.Contains(station));
+                cache[town][sta] = Tuple.Create(veloLibre, DateTime.Now.Second);
+            }
             return veloLibre;
         }
 
         private int getAvailableBikesService(string town, string station)
         {
-            WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + town + "&apiKey=" + API_KEY);
+        
+                WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + town + "&apiKey=" + API_KEY);
+                WebResponse reponse = requete.GetResponse();
+                Stream stream = reponse.GetResponseStream();
 
-            WebResponse reponse = requete.GetResponse();
-            Stream stream = reponse.GetResponseStream();
-
-
-            StreamReader reader = new StreamReader(stream);
-            string restr = reader.ReadToEnd();
-
-            reponse.Close();
-            reader.Close();
-
-            JArray jsonArray = JArray.Parse(restr);
-            int size = jsonArray.Count;
-
-            foreach (JObject item in jsonArray)
-            {
-                if (((string)item["name"]).ToLower().Contains(station.ToLower()))
+                StreamReader reader = new StreamReader(stream);
+                string restr = reader.ReadToEnd();
+                if (reponse == null || station == null)
                 {
-                    int veloLibre = Convert.ToInt32(item["available_bikes"]);
-                    cache[town][station] = Tuple.Create(veloLibre, cache[town][station].Item2);
-                    return veloLibre;
+                    return -1;
                 }
-            }
+                reponse.Close();
+                reader.Close();
 
+                JArray jsonArray = JArray.Parse(restr);
+                int size = jsonArray.Count;
+                foreach (JObject item in jsonArray)
+                {
+                    string name = ((string)item["name"]).ToLower();
+             
+                    if (item["name"] == null)
+                        return -1;
+                    if (!cache[town].ContainsKey(name))
+                    {
+                        cache[town].Add(name, Tuple.Create(-1, -1));
+                    }
+                    if (((string)item["name"]).ToLower().Contains(station.ToLower()))
+                    {
+                        int veloLibre = Convert.ToInt32(item["available_bikes"]);
+                        cache[town][name] = Tuple.Create(veloLibre, cache[town][name].Item2);
+                        return veloLibre;
+                    }
+                }
+         
             return -1;
         }
 
         private IList<string> getTownsService()
         {
-            IList<string> towns = new List<string>();
-
-            WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/contracts?apiKey=" + API_KEY);
-
-
-            WebResponse reponse = requete.GetResponse();
-            Stream stream = reponse.GetResponseStream();
-
-
-            StreamReader reader = new StreamReader(stream);
-            string restr = reader.ReadToEnd();
-
-            reponse.Close();
-            reader.Close();
-
-            JArray jsonArray = JArray.Parse(restr);
-
-            foreach (JObject item in jsonArray)
+            try
             {
-                cache.Add(item["name"].ToString() , new Dictionary<string,Tuple<int,int>>());
-                towns.Add(item["name"].ToString());
+                IList<string> towns = new List<string>();
+
+                WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/contracts?apiKey=" + API_KEY);
+
+
+                WebResponse reponse = requete.GetResponse();
+                if (reponse == null)
+                {
+                    return null;
+                }
+                Stream stream = reponse.GetResponseStream();
+
+
+                StreamReader reader = new StreamReader(stream);
+                string restr = reader.ReadToEnd();
+
+                reponse.Close();
+                reader.Close();
+
+                JArray jsonArray = JArray.Parse(restr);
+
+                foreach (JObject item in jsonArray)
+                {
+
+                    cache.Add(item["name"].ToString().ToLower(), new Dictionary<string, Tuple<int, int>>());
+                    towns.Add(item["name"].ToString().ToLower());
+                }
+                return towns;
+            }
+            catch(Exception e)
+            {
+                return null;
             }
 
-            return towns;
+           
         }
 
         private IList<string> getStationsService(string town)
         {
-            IList<string> stationsOfTown = new List<string>();
-
-            WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + town + "&apiKey=" + API_KEY);
-
-            WebResponse reponse = requete.GetResponse();
-            Stream stream = reponse.GetResponseStream();
-
-            StreamReader reader = new StreamReader(stream);
-            string restr = reader.ReadToEnd();
-
-            reponse.Close();
-            reader.Close();
-
-            JArray jsonArray = JArray.Parse(restr);
-
-            foreach (JObject item in jsonArray)
+            try
             {
-                cache[town].Add(item["name"].ToString(), Tuple.Create(-1,-1));
-                stationsOfTown.Add(item["name"].ToString());
-            }
+                IList<string> stationsOfTown = new List<string>();
 
-            return stationsOfTown;
+                WebRequest requete = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + town + "&apiKey=" + API_KEY);
+
+                WebResponse reponse = requete.GetResponse();
+                if (reponse == null)
+                {
+                    return null;
+                }
+                Stream stream = reponse.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string restr = reader.ReadToEnd();
+
+                reponse.Close();
+                reader.Close();
+
+                JArray jsonArray = JArray.Parse(restr);
+
+                foreach (JObject item in jsonArray)
+                {
+                    cache[town].Add(item["name"].ToString().ToLower(), Tuple.Create(-1, -1));
+                    stationsOfTown.Add(item["name"].ToString().ToLower());
+                }
+                return stationsOfTown;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+         
         }
 
         public IAsyncResult BegingetStations(string City, AsyncCallback callback, object state)
