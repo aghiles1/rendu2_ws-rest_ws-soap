0d31cd572ef3ada3888851d35c9f1ae5af38384d
Aghiles dziri <aghiles_dzii@hotmail.fr>
2018-04-05 11:12:56 +0200

    donner le nom de la ville et le nom de la station avec un readline

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,12 +11,16 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merci de donner une ville pour le subscribe");
+            string town = Console.ReadLine();
+            Console.WriteLine("Merci de donner une stations avec le nom complet pour le subscribe");
+            string station = Console.ReadLine();
             CalServiceCallbackSink objsink = new CalServiceCallbackSink();
             InstanceContext iCntxt = new InstanceContext(objsink);
             ServiceEvent.CalServiceClient objClient = new ServiceEvent.CalServiceClient(iCntxt);
             objClient.SubscribeCalculatedEvent();
             objClient.SubscribeCalculationFinishedEvent();
-            objClient.Calculate("nantes", "alger",0);
+            objClient.Calculate(town, station, 0);
             Console.WriteLine("Press any key to close ...");
             Console.ReadKey();
         }
